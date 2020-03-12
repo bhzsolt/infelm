cc32392b6871fe5aa7315bdb97b636de9c7e0ca8
Bodoki-Halmen Zsolt <bokodihalmen@yahoo.com>
2020-03-12 11:20:32 +0200

    added c01 and corpus for lab01

---
+++ c01/c01.py
@@ -0,0 +1,92 @@
+#!/bin/python3
+# Bodoki-Halmen Zsolt
+# bzim1700
+# 531/1
+
+from sys import argv
+from os import path
+from math import log2
+
+def error():
+    print('usage: {} prob_file'.format(argv[0]))
+    exit(1)
+
+def prob(p):
+    return 0.0 <= p and p <= 1.0
+
+def interp_arg():
+    if len(argv) < 2:
+        error()
+
+    file_name = argv[1]
+    if not path.isfile(file_name):
+        print('{} is not a file'.format(file_name))
+        error()
+
+    with open(file_name, 'r') as f:
+        lines = f.read().strip().split('\n')
+
+    px = float(lines[0])
+    py0 = float(lines[1])
+    py1 = float(lines[2])
+
+    if len(lines) != 3 or not prob(px) or not prob(py0) or not prob(py1):
+        print('{} not a valid prob_file'.format(file_name))
+        error()
+
+    return px, py0, py1
+
+def entropy(A):
+    return A[0] * log2(1 / A[0]) + A[1] * log2(1 / A[1])
+
+def cond_entropy_x(A, x):
+    return A[x][0] * log2(1 / A[x][0]) + A[x][1] * log2(1 / A[x][1])
+
+def cond_entropy(A, B):
+    return B[0] * cond_entropy_x(A, 0) + B[1] * cond_entropy_x(A, 1)
+
+def mutual_prob(A, B, a, b):
+    return A[b][a] * B[b]
+
+def mutual_entropy(A, B):
+    H = 0.0
+    for x in range(2):
+        for y in range(2):
+            H += mutual_prob(A, B, x, y) * log2(1 / mutual_prob(A, B, x, y))
+    return H
+
+def relative_entropy(A, B):
+    return A[0] * log2(A[0] / B[0]) + A[1] * log2(A[1] / B[1])
+
+def mutual_information(A_, A, B):
+    I = 0.0
+    for x in range(2):
+        for y in range(2):
+            I += mutual_prob(A_, B, x, y) * log2(mutual_prob(A_, B, x, y) / (A[x] * B[y]))
+    return I
+
+################################################################################
+
+px, py0, py1 = interp_arg()
+py = py0 * (1-px) + py1 * px
+
+X = (1-px, px)
+Y = (1-py, py)
+Y0 = (1-py0, py0)
+Y1 = (1-py1, py1)
+Y_ = (Y0, Y1)
+px0 = mutual_prob(Y_, X, 0, 1)/Y[0]
+px1 = mutual_prob(Y_, X, 1, 1)/Y[1]
+X0 = (1-px0, px0)
+X1 = (1-px1, px1)
+X_ = (X0, X1)
+
+
+print('H(X)	= {:6.4f}'.format(entropy(X)))
+print('H(Y)	= {:6.4f}'.format(entropy(Y)))
+print('H(X,Y)	= {:6.4f}'.format(mutual_entropy(Y_, X)))
+print('H(X|Y)	= {:6.4f}'.format(cond_entropy(X_, Y)))
+print('H(Y|X)	= {:6.4f}'.format(cond_entropy(Y_, X)))
+print('I(X,Y)	= {:6.4f}'.format(mutual_information(Y_, Y, X)))
+print('D(X||Y)	= {:6.4f}'.format(relative_entropy(X, Y)))
+print('D(Y||X)	= {:6.4f}'.format(relative_entropy(Y, X)))
--- lab02/bmp_rle.py
+++ lab02/bmp_rle.py
@@ -3,4 +3,88 @@
 # bzim1700
 # 531/1
 
+import struct
+from sys import argv
+from os import path
+from functools import reduce
 
+#print('Type:', bmp.read(2).decode()) [0:2]
+#print('Size: %s' % struct.unpack('I', bmp.read(4))) [2:6]
+#print('Reserved 1: %s' % struct.unpack('H', bmp.read(2))) [6:8]
+#print('Reserved 2: %s' % struct.unpack('H', bmp.read(2))) [8:10]
+#print('Offset: %s' % struct.unpack('I', bmp.read(4))) [10:14]
+
+#print('DIB Header Size: %s' % struct.unpack('I', bmp.read(4))) [14:18]
+#print('Width: %s' % struct.unpack('I', bmp.read(4))) [18:22]
+#print('Height: %s' % struct.unpack('I', bmp.read(4))) [22:26]
+#print('Colour Planes: %s' % struct.unpack('H', bmp.read(2))) [26:28]
+#print('Bits per Pixel: %s' % struct.unpack('H', bmp.read(2))) [28:30]
+#print('Compression Method: %s' % struct.unpack('I', bmp.read(4))) [30:34]
+#print('Raw Image Size: %s' % struct.unpack('I', bmp.read(4))) [34:38]
+#print('Horizontal Resolution: %s' % struct.unpack('I', bmp.read(4))) [38:42]
+#print('Vertical Resolution: %s' % struct.unpack('I', bmp.read(4))) [42:46]
+#print('Number of Colours: %s' % struct.unpack('I', bmp.read(4))) [46:50]
+
+def rle_encode(decoded, encoded):
+    with open(decoded, 'rb') as bmp:
+        header = bmp.read(54)
+        raw = bmp.read()
+        print(type(raw))
+
+    flat = []
+    p = b'0'
+    first = True
+    c = 0
+    for x in raw:
+        if x != p:
+            if not first:
+                flat.append((p, c))
+            c = 1
+        else:
+            c += 1
+        p = x
+        first = False
+    flat.append((x, c))
+
+    with open(encoded, 'wb') as f:
+        f.write(header)
+        f.write(struct.pack('I', len(flat)))
+        for x, c in flat:
+            f.write(struct.pack('B', x))
+            f.write(struct.pack('H', c))
+
+def rle_decode(encoded, decoded):
+    with open(encoded, 'rb') as f:
+        header = f.read(54)
+        length = struct.unpack('I', f.read(4))[0]
+        flat = []
+        for i in range(length):
+            x = f.read(1)
+            c = struct.unpack('H', f.read(2))[0]
+            flat.append((x,c))
+    real = []
+    for x, c in flat:
+        for i in range(c):
+            real.append(x)
+
+    with open(decoded, 'wb') as bmp:
+        bmp.write(header)
+        for x in real:
+            bmp.write(x)
+
+AND = lambda x,y: x and y
+check_extension = lambda x: path.splitext(x)[1].lower() == '.bmp'
+
+################################################################################
+
+if (len(argv) < 2 
+        or not reduce(AND, map(path.isfile, argv[1:]))
+        or not reduce(AND, map(check_extension, argv[1:]))):
+    print('usage: {} [file.bmp]'.format(argv[0]))
+    exit(1)
+
+images = argv[1:]
+
+for image in images:
+    rle_encode(image, image+'.bh')
+    rle_decode(image+'.bh', image+'.bh.bmp')
